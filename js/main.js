27f4e2b773fd20ea4c91718911a00370917fab44
diadiaspora <dia.diaspora@gmail.com>
2025-04-25 16:41:18 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/*----- psuedocode -----*/
+/* 
+- Display empty word placeholder that needs to be guess.
+- Player can click on letters to add to placeholder.
+- Incorrect letter is stored to right when it is pressed but does not work for the given word.
+- Food item taken from plate with incorrect guess.
+- Correct letter is added to placeholder.
+- Player is given a 'win' confirmation when all placeholder for letters are filled.
+- Choose new word button if game is unfinished.
+- Reset game button when gave is finished.
+*\
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+let placeholder;
+let secretWord; /=== eachother when won/
+let wrongLetter
+
+
+/*----- cached elements  -----*/
+
+keys/buttons - 
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
